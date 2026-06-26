c21e0bc3b4507ffe24cc2190701a6ee47c42a2c8
Liam <liamgoodchild12@hotmail.co.uk>
2026-06-26 17:04:08 +0100

    Remove unused Key Vault and its access grants

--- infra/rbac.tf
+++ infra/rbac.tf
@@ -1,15 +1,3 @@
-resource "azurerm_role_assignment" "deployer_kv_secrets_officer" {
-  scope                = azurerm_key_vault.main.id
-  role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
-
-resource "azurerm_role_assignment" "function_kv_secrets_user" {
-  scope                = azurerm_key_vault.main.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_function_app_flex_consumption.main.identity[0].principal_id
-}
-
 resource "azurerm_role_assignment" "function_storage_blob" {
   scope                = azurerm_storage_account.functions.id
   role_definition_name = "Storage Blob Data Owner"
