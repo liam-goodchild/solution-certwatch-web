a10f4eb5f718015b6c157b611ec8c9701c9f27bd
Liam <liamgoodchild12@hotmail.co.uk>
2026-06-26 16:24:42 +0100

    Upgrade Static Web App to Standard SKU for custom auth

--- infra/static-web-app.tf
+++ infra/static-web-app.tf
@@ -2,8 +2,8 @@ resource "azurerm_static_web_app" "main" {
   name                = "stapp-${local.resource_suffix}"
   resource_group_name = azurerm_resource_group.main.name
   location            = "westeurope"
-  sku_tier            = "Free"
-  sku_size            = "Free"
+  sku_tier            = "Standard"
+  sku_size            = "Standard"
 
   app_settings = {
     ENTRA_CLIENT_ID     = var.entra_api_client_id
